76eb1a74ab07a3cb55d8652e810e96ec88d8fdf5
Sungjae Park <park1637@neoprene.morris.umn.edu>
2017-11-09 11:32:49 -0600

    write code but fail the test

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -8,10 +8,39 @@ import java.net.Socket;
 import java.io.*;
 import java.net.*;
 
-public class EchoClient {
-	public static void main(String [] args) {
-		try{
-			//connect to server
-			socket = new Socket("!27.0.0.1" , 6013);
-			System.out.println("Success to connect!");
-			
+public class EchoClient extends Thread {
+	
+	public static final int PORT_NUMBER = 6013;
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		EchoClient client  = new EchoClient();
+		client.start();
+	}	
+
+	class ClientThread extends Thread {
+		private Socket sock;
+		public ClientThread(Socket socke) {
+			this.sock = socke;
+		}
+		public void run(){
+			try{
+				OutputStream out = sock.getOutputStream();
+				InputStream in = sock.getInputStream();
+
+				int NewType;
+				while((NewType = in.read()) != -1) {
+					out.write(NewType);
+				}
+
+				out.flush();
+				sock.close();
+				System.out.println("Disconnected!");
+
+			} catch (IOException ioe) {
+				System.out.println("ERRORS!");
+				System.out.println(ioe);
+			}
+		}
+	}
+}
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,8 +5,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
-public class EchoServer {
+
+public class EchoServer extends Thread {
 	
 	public static final int PORT_NUMBER = 6013;
 
@@ -16,25 +19,33 @@ public class EchoServer {
 		server.start();
 	}
 
-	class EchoThread extends Thread{
+	class EchoThread extends Thread {
 		private Socket sock;
-		public EchoThread(socket socke){
+		public EchoThread(Socket socke){
 			this.sock = socke;
 		}
-
+		
 		public void run() {
-			try{
+			try {
 				OutputStream out = sock.getOutputStream();
-				InputStrema in = sock.getInputStream();
+				InputStream in = sock.getInputStream();
 
+				int NewType;
+				while((NewType = in.read()) != -1) {
+					out.write(NewType);
+
+				}
+				out.flush();
+				sock.close();
+				System.out.println("Disconnected!");
+			
+			} catch (IOException ioe) {
+				System.out.println("ERROR!");
+				
+				System.out.println(ioe);
 			}
-			sock.close();
-			System.out.println("Disconnected!");
-		} catch(IOException e){
-			System.out.println(e);
 		}
 	}
 }
-
 
 
