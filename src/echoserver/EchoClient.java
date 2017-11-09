74ad2a58c07dface821701deb96c78d5549c2a9f
Sungjae Park <park1637@neoprene.morris.umn.edu>
2017-11-09 09:32:30 -0600

    keep working on Echo Client

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,28 +1,58 @@
 package echoserver;
 
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class EchoClient {
-	public static final int PORT_NUMBER = 6013;
+	public static void main(String [] args) {
+		Socket socket = null;
+		InputStream in = null;
+		OutputStream out = null;
+		BufferedReader reader = null;
+		PrintWriter writer = null;
+		BufferedReader stdin = null;
+		try{
+			//connect to server
+			socket = new Socket("!27.0.0.1" , 6013);
+			System.out.println("Success to connect!");
+			
+			//stream for server communication
+			in = socket.getInputStream();
+			out = socket.getOutputStream();
+			reader = new BufferedReader(new InputStreamReader(in));
+			writer = new PrintWriter(new OutputStreamWriter(out));
+			stdin = new BufferedReader(new InputStreamReader(System.in));
+			String msg = null;
+			String echo = null;
 
-	public static void main(String[] args) throws IOException {
-		EchoClient client = new EchoClient();
-		client.start();
-	}
-
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
+			// communicate with server
+			while((msg = stdin.readLine()) !=null) {
+				writer.println(msg);
+				writer.flush();
+				echo = reader.readLine();
+				System.out.println("$tSERVER>"+echo);
+			}
+		} catch(IOException e) {
+			System.out.println(e);
+		}finally {
+			try{
+				stdin.close();
+				reader.close();
+				writer.close();
+				in.close();
+				out.close();
+				socket.close();
+				System.out.println("Disconnected!");
+			} catch(IOException e) {
+				System.out.println(e);
+			}
 		}
-		System.out.flush();
 	}
 }
+
+
