c76615a5fcc73c515250631e18946c5501ca05c6
Sungjae Park <park1637@neoprene.morris.umn.edu>
2017-11-16 11:46:40 -0600

    chage

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -8,15 +8,49 @@ import java.net.Socket;
 import java.io.*;
 import java.net.*;
 
-public class EchoClient extends Thread {
+public class EchoClient {
 	
 	public static final int PORT_NUMBER = 6013;
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-		EchoClient client  = new EchoClient();
+		ClientThread client  = new ClientThread();
 		client.start();
-	}	
+	}
+
+class clientThread extends Thread{
+	public static int myCount = 0;
+	public void run(){
+		while(clientThread <=10){
+			try{
+				System.out.println("Exp1 Thread: " +(++clientThread.myCount));
+				Thread.sleep(100);
+			}catch (IOException e) {
+				System.out.println("Exception in thread: "+e.getMessage());
+			}
+		}
+	}
+}
+
+class countingThread extends Thread{
+	public int myCount =0;
+	public int start = 0;
+	public int stop = 0;
+
+	public countingThread(int start, int stop){
+		this.start=start;
+
+	}
+	
+	public void run(){
+		for(int i = 0;, i<20, i++) {
+			
+		try{ 
+			OutputStream out = start.getOutputStream();
+			InputStream in = start.getInputStream();
+
+			int count;
+			while((
 
 	class ClientThread extends Thread {
 		private Socket sock;
@@ -37,10 +71,24 @@ public class EchoClient extends Thread {
 				sock.close();
 				System.out.println("Disconnected!");
 
-			} catch (IOException ioe) {
+			} catch (IOException e) {
 				System.out.println("ERRORS!");
-				System.out.println(ioe);
+				System.out.println(e);
 			}
 		}
 	}
 }
+
+
+
+
+//	private void start() throws IOException {
+//		Socket sock = new Socket("localhost", 6013);
+//		InputThread inThread = new InputThread(sock);
+//		OutputThread outThread = new OutputThread(sock);
+//
+//		inThread.start();
+//		outThread.start();
+//	}
+//}
+
